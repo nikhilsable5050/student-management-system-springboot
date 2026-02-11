4850b8ae7c7bbb2b503a9c22cc7ae6ac42c86f34
nikhisable5050 <nikhilsable5050@gmail.com>
2026-02-11 15:22:17 +0530

    feat: add admin user initializer on application startup

--- student-management-system/src/main/java/com/nikhilsable/studentmanagementsystem/config/DataInitializer.java
+++ student-management-system/src/main/java/com/nikhilsable/studentmanagementsystem/config/DataInitializer.java
@@ -1,4 +1,25 @@
 package com.nikhilsable.studentmanagementsystem.config;
 
+import com.nikhilsable.studentmanagementsystem.entity.Users;
+import com.nikhilsable.studentmanagementsystem.repository.UsersRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
 public class DataInitializer {
+    @Bean
+    CommandLineRunner loadSampleData(UsersRepository usersRepository,
+                                     PasswordEncoder passwordEncoder){
+        return args -> {
+            if(!usersRepository.existsByUsername("Admin")) {
+                Users users = new Users();
+                users.setUsername("Admin");
+                users.setPassword(passwordEncoder.encode("admin@123"));
+                users.setActive(true);
+                usersRepository.save(users);
+            }
+        };
+    }
 }
