7684c3b83aa050b41b395f9bfc994e69a209fb40
nikhisable5050 <nikhilsable5050@gmail.com>
2026-02-12 15:29:17 +0530

    Implement UserDetailsService for database authentication

--- student-management-system/src/main/java/com/nikhilsable/studentmanagementsystem/repository/UsersRepository.java
+++ student-management-system/src/main/java/com/nikhilsable/studentmanagementsystem/repository/UsersRepository.java
@@ -3,6 +3,11 @@ package com.nikhilsable.studentmanagementsystem.repository;
 import com.nikhilsable.studentmanagementsystem.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UsersRepository extends JpaRepository<Users,Long> {
     boolean existsByUsername(String username);
+
+    @Override
+    Optional<Users> findByUsername(String username);
 }
---
+++ student-management-system/src/main/java/com/nikhilsable/studentmanagementsystem/service/impl/UserServiceImpl.java
@@ -0,0 +1,28 @@
+package com.nikhilsable.studentmanagementsystem.service.impl;
+
+import com.nikhilsable.studentmanagementsystem.entity.Users;
+import com.nikhilsable.studentmanagementsystem.repository.UsersRepository;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserDetailsService {
+
+    private UsersRepository usersRepository;
+    public UserServiceImpl(UsersRepository usersRepository){
+        this.usersRepository = usersRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Users users = usersRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Invalid username"));
+        return User.withUsername(username)
+                .password(users.getPassword())
+                .disabled(!users.isActive())
+                .build();
+    }
+}
